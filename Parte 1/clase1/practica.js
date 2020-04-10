6b108fcf41e03cdcc635b2b90a9d40e496d3a4a8
Luis Hurtado <luis.hurtado@linio.com>
2020-04-10 10:48:43 -0400

    Practicas react

---
+++ Parte 1/clase1/practica.js
@@ -0,0 +1,17 @@
+class Tarea{
+
+    constructor(nombre, prioridad){
+        this.nombre = nombre;
+        this.prioridad = prioridad;
+    }
+
+    
+
+}
+
+
+//crear objetos
+
+let tarea1 = new Tarea('Aprender javaScript', 'Alta');
+
+console.log(tarea1);
